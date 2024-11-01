4abb0be6b8cc8906fb719cadbe2d82b1abe2de2a
divy404 <divyanshj360@gmail.com>
2024-11-01 19:56:01 +0530

    03

--- notes.js
+++ notes.js
@@ -1,2 +1,9 @@
 // npm stands for node package manager
 // npx stands for node package executer
+
+// hooks
+// useState
+
+
+// https://github.com/facebook/react/blob/main/packages/react-dom/src/client/ReactDOMClient.js
+// https://github.com/acdlite/react-fiber-architecture 
